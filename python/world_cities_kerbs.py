a3f8520bd515449fe31017db4233baa94ad9a58b
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-08-04 13:02:51 +0100

    Initial attempt at identifying clusters of kerbs - patches of a city where kerbs have been mapped

--- python/world_cities_kerbs.py
+++ python/world_cities_kerbs.py
@@ -1,12 +1,16 @@
 # Scrape OSM data for multiple cities
 
 import os
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 import osmnx as ox
+import networkx as nx
+import itertools
 
 import osm_streetspace_utils as ossutils
 
+from matplotlib import pyplot as plt
 
 ################################
 #
@@ -19,6 +23,9 @@ import osm_streetspace_utils as ossutils
 merc_crs = {'init' :'epsg:3857'}
 output_dir = "..//data//world"
 kerb_data_filename = "kerb_data.gpkg"
+
+world_kerb_lengths_file = os.path.join(output_dir, "world_city_kerb_totals.csv")
+rhoads_kerb_lengths_file = os.path.join(output_dir, "rhoads_city_kerb_totals.csv")
 
 
 ################################
@@ -81,18 +88,41 @@ def save_city_data(dict_city_data, output_dir = output_dir, filename = kerb_data
 
 	return True
 
+def load_city_kerb_data(cities, output_dir, project_crs = merc_crs, filename = kerb_data_filename):
+	city_kerbs = {}
+
+	for city_name in cities:
+		gdfCityKerb = gpd.GeoDataFrame()
+		city_data_path = os.path.join(output_dir, city_name, filename)
+		if os.path.exists(city_data_path)==False:
+			note_path = os.path.join(output_dir, city_name, 'note.txt')
+			note = None
+			with open(note_path, 'r') as f:
+				note = f.readline()
+			print("{}: {}".format(city_name, note))
+
+			if 'Empty dataframe' in note:
+				city_kerbs[city_name] = gdfCityKerb
+			continue
+
+		gdfCityKerb = gpd.read_file(city_data_path)
+		gdfCityKerb = gdfCityKerb.to_crs(project_crs)
+		city_kerbs[city_name] = gdfCityKerb
+
+	return city_kerbs
+
 def city_kerb_length_totals(cities, output_dir, project_crs = merc_crs, filename = kerb_data_filename):
 
 	totals = {'city':[], 'kerb_length':[], 'city_area':[]}
 
-	for city_name in cities:
-		city_data = os.path.join(output_dir, city_name, filename)
-		if os.path.exists(city_data)==False:
-			print("Path not found for {}".format(city_name))
-			continue
+	city_kerbs = load_city_kerb_data(cities, output_dir, project_crs, filename)
 
-		gdfCityKerb = gpd.read_file(city_data)
-		gdfCityKerb = gdfCityKerb.to_crs(project_crs)
+	for city_name, gdfCityKerb in city_kerbs.items():
+		if gdfCityKerb.shape[0]==0:
+			totals['city'].append(city_name)
+			totals['kerb_length'].append(0)
+			totals['city_area'].append(np.nan)
+			continue
 
 		gdfCityKerb['length'] = gdfCityKerb['geometry'].length
 
@@ -101,6 +131,82 @@ def city_kerb_length_totals(cities, output_dir, project_crs = merc_crs, filename
 		totals['city_area'].append(gdfCityKerb['bb_area'].unique()[0])
 
 	return pd.DataFrame(totals)
+
+def city_kerb_pairwise_distances(gdfCityKerbs):
+	'''Build nearest neighbour network by pairing row IDs if their geometries are within distance d of each other.
+	'''
+
+	edges = []
+
+	ids = gdfCityKerbs.index
+	
+	# Do pairwise comparison between all row IDs
+	for u, v in itertools.product(ids, repeat=2):
+		gu = gdfCityKerbs.loc[u, 'geometry']
+		gv = gdfCityKerbs.loc[v, 'geometry']
+		duv = gu.distance(gv)
+		edges.append((u,v,duv))
+
+	dfEdges = pd.DataFrame(edges, columns = ['u', 'v', 'd'])
+	return dfEdges
+
+
+def multiple_cities_kerb_pairwise_distances(cities, output_dir, project_crs = merc_crs, filename = kerb_data_filename):
+
+	city_pairwise_dist = {}
+
+	city_kerbs = load_city_kerb_data(cities, output_dir, project_crs, filename)
+
+	for city_name, gdfCityKerbs in city_kerbs.items():
+		if gdfCityKerbs.shape[0]==0:
+			city_pairwise_dist[city_name] = None
+		else:
+			dfPairDists = city_kerb_pairwise_distances(gdfCityKerbs)
+			city_pairwise_dist[city_name] = dfPairDists
+
+	return city_pairwise_dist
+
+def city_kerb_clusters_from_pairwise_distances(city_pairwise_dist, d_threshold = 10):
+
+	clusters = {'city':[], 'nclusters':[]}
+	for city_name, dfPairDists in city_pairwise_dist.items():
+
+		if dfPairDists is None:
+			continue
+
+		# Build network of kerbs that are within distance d of each other
+		dfPairDists = dfPairDists.loc[ dfPairDists['d'] < d_threshold ]
+		dfPairDists['data'] = dfPairDists['d'].map(lambda x: {'d':x})
+		edges = dfPairDists.loc[:, ['u','v','data']].values
+
+		g = nx.Graph()
+		g.add_edges_from(edges)
+
+		ccs = nx.connected_components(g)
+
+		nclusters = 0
+		for cc in ccs:
+			nclusters+=1
+
+			# Also need to calculate total kerb length of cluster
+
+		clusters['city'].append(city_name)
+		clusters['nclusters'].append(nclusters)
+
+	return clusters
+
+
+def kerb_bar_chart(series, series_label, ylabel, title, img_path):
+    f, ax = plt.subplots(figsize = (10,10))
+    p1 = ax.bar(series.index, series, 0.9, label=series_label)
+    ax.axhline(0, color='grey', linewidth=0.8)
+    ax.set_ylabel(ylabel)
+    ax.set_xticks(range(len(series.index)))
+    ax.set_xticklabels(series.index)
+    ax.set_title(title)
+    plt.xticks(rotation=30, ha='right')
+    f.savefig(img_path)
+    return f, ax
 
 
 ############################
@@ -118,10 +224,8 @@ city_kerbs = get_kerbs_for_multiple_cities(cities)
 save_city_data(city_kerbs)
 
 # Now calculate total length covered by kerbs
-dfKerbLengths = city_kerb_length_totals(cities, output_dir = output_dir)
-dfKerbLengths.to_csv(os.path.join(output_dir, "rhoads_city_kerb_totals.csv"), index=False)
-
-
+dfKerbLengthsRhoads = city_kerb_length_totals(rhoads_cities, output_dir = output_dir)
+dfKerbLengthsRhoads.to_csv(rhoads_kerb_lengths_file, index=False)
 
 
 # Load world cities data, access from https://data.london.gov.uk/dataset/global-city-population-estimates
@@ -142,4 +246,35 @@ save_city_data(city_kerbs)
 
 # Now calculate total length covered by kerbs
 dfKerbLengths = city_kerb_length_totals(cities, output_dir = output_dir)
-dfKerbLengths.to_csv(os.path.join(output_dir, "world_city_kerb_totals.csv"), index=False)
+dfKerbLengths.to_csv(world_kerb_lengths_file, index=False)
+
+
+##############################
+#
+#
+# Identify clusters of kerbs
+#
+#
+##############################
+
+city_pairwise_dist = multiple_cities_kerb_pairwise_distances(rhoads_cities, output_dir, project_crs = merc_crs, filename = kerb_data_filename)
+city_cluster_data = city_kerb_clusters_from_pairwise_distances(city_pairwise_dist, d_threshold = 20)
+
+dfRhoadsClusters = pd.DataFrame(city_cluster_data)
+
+dfKerbLengthsRhoads = pd.merge(dfKerbLengthsRhoads, dfRhoadsClusters, on = 'city')
+dfKerbLengthsRhoads['kerb_length_per_cluster'] = dfKerbLengthsRhoads['kerb_length'] / dfKerbLengthsRhoads['nclusters']
+
+##############################
+#
+#
+# Visualise results
+#
+#
+##############################
+
+kerb_lengths = dfKerbLengthsRhoads.set_index('city')['kerb_length'].sort_values(ascending=False)
+f, ax = kerb_bar_chart(kerb_lengths, 'Total OSM Kerb Length', 'm', 'Total OSM Kerb Length', "../images/rhoads_kerb_lengths.png")
+
+kerb_lengths_per_c = dfKerbLengthsRhoads.set_index('city')['kerb_length_per_cluster'].sort_values(ascending=False)
+f, ax = kerb_bar_chart(kerb_lengths_per_c, 'OSM Kerb Length Per Cluster', 'm', 'OSM Kerb Length Per Cluster', "../images/rhoads_kerb_lengths_per_cluster.png")
