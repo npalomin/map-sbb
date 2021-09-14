c7b7cfc0c7d677edaa658e352835fe6b7fecf7ea
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-09-14 18:30:47 +0100

    Producing figures and reorganising analysis for clarity.

--- python/world_cities_kerbs.py
+++ python/world_cities_kerbs.py
@@ -201,6 +201,16 @@ def city_kerb_clusters_from_pairwise_distances(city_pairwise_dist, d_threshold =
 
     return clusters
 
+def silhouette_scores(mink, maxk, reps, distances):
+    scores = {'k':[], 's':[]}
+    for repetitions in range(reps):
+        for k in range(mink, maxk+1):
+            kmedoids = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+            s = silhouette_score(distances, kmedoids.labels_, metric='precomputed', sample_size=None, random_state=None)
+            scores['k'].append(k)
+            scores['s'].append(s)
+    dfScores = pd.DataFrame(scores).groupby('k').mean().reset_index()
+    return dfScores
 
 ###############################
 #
@@ -332,8 +342,6 @@ dfCityKerbs['new_geometry'] = dfCityKerbs['geometry'].map(lambda g: closed_lines
 
 # Record geometry type as 1 hot encoding, will drop Points, so only need to record if polygon or not
 # Also record area of shape and length
-
-dfCityKerbs['is_polygon'] = dfCityKerbs['new_geometry'].map(lambda g: g.type == 'Polygon')
 dfCityKerbs['osm_length'] = dfCityKerbs['new_geometry'].length
 dfCityKerbs['osm_area'] = dfCityKerbs['new_geometry'].area
 
@@ -376,7 +384,7 @@ dfCityKerbsTags['osm_length_mm'] = (dfCityKerbsTags['osm_length'] - dfCityKerbsT
 dfCityKerbsTags['osm_area_mm'] = (dfCityKerbsTags['osm_area'] - dfCityKerbsTags['osm_area'].min()) / (dfCityKerbsTags['osm_area'].max() - dfCityKerbsTags['osm_area'].min())
 
 # Filter to just columns to use in clustering
-categorical_cols = ['is_polygon', 'area_name']
+categorical_cols = ['area_name']
 numerical_cols = ['osm_length_mm','osm_area_mm']
 
 
@@ -399,10 +407,6 @@ def gower_distance(df, tags_cols, categorical_cols, numerical_cols):
 
         if c in categorical_cols:
             feature_dist = DistanceMetric.get_metric('dice').pairwise(pd.get_dummies(feature))
-
-            if c == 'is_polygon':
-                # get nan from dividing by zero when 
-                feature_dist = np.where(pd.isna(feature_dist), 0, feature_dist)
         else:
             feature_dist = DistanceMetric.get_metric('manhattan').pairwise(feature) / max(np.ptp(feature.values),1)
         individual_variable_distances.append(feature_dist)
@@ -410,31 +414,100 @@ def gower_distance(df, tags_cols, categorical_cols, numerical_cols):
     return np.array(individual_variable_distances).mean(0)
 
 dfCityKerbsTags.index = np.arange(dfCityKerbsTags.shape[0])
-distances = gower_distance(dfCityKerbsTags, tags_cols, categorical_cols, numerical_cols)
 
 
 # Now cluster with these distances and calculate cluster silhouettes
+import sklearn
 from sklearn_extra.cluster import KMedoids
 from sklearn.metrics import silhouette_score
 
-'''
-scores = {'k':[], 's':[]}
-for repetitions in range(5):
-    for k in range(2, 11):
-        kmedoids = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
-        s = silhouette_score(distances, kmedoids.labels_, metric='precomputed', sample_size=None, random_state=None)
-        scores['k'].append(k)
-        scores['s'].append(s)
-dfScores = pd.DataFrame(scores).groupby('k').mean().reset_index()
-dfScores.to_csv("silhouette_scores.csv", index=False)
-'''
+
+# Look at correlations to see if these are preventing logit model from converging
+from matplotlib import pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import seaborn as sn
+
+#
+# Get different distance matrices that make use of different combinations of variables
+#
+distances = gower_distance(dfCityKerbsTags, tags_cols, categorical_cols, numerical_cols)
+
+distances_no_name = gower_distance(dfCityKerbsTags, tags_cols, [], numerical_cols)
+
+distances_no_tags = gower_distance(dfCityKerbsTags, None, categorical_cols, numerical_cols)
+
+distances_numeric = gower_distance(dfCityKerbsTags, None, [], numerical_cols)
+
+distances_tags = gower_distance(dfCityKerbsTags, tags_cols, [], [])
+distances_tags_cos = sklearn.metrics.pairwise.cosine_distances(dfCityKerbsTags[tags_cols].values) # both give 3 clusters as lowest s score
+
+#
+# Silhouette scores for different distance matrices
+#
+dfScores = silhouette_scores(2, 10, 3, distances) # 2 clusters best
+dfSNoName = silhouette_scores(2, 10, 3, distances_no_name) # 4 clusters best
+dfSNoTags = silhouette_scores(2, 10, 3, distances_no_tags) # 2 clusters best
+dfSNumeric = silhouette_scores(2, 10, 3, distances_numeric) # Bit unclear, 3 or 7. Higher silhouette scores than for other sets of attributes.
+dfSTags = silhouette_scores(2, 10, 3, distances_tags) # 3 clusters
+dfSTagsCos = silhouette_scores(2, 10, 3, distances_tags_cos) # 3 clusters
+
+# Merge together
+dfSil = pd.merge(dfScores, dfSNumeric, on = 'k', suffixes = ('', '_numeric'))
+dfSil = pd.merge(dfSil, dfSTags, on = 'k', suffixes = ('', '_tags'))
+dfSil.to_csv('silhouette_scores.csv', index=False)
+
+
+#
+# SSE of clusters - cant get bc of pam and kedoids
+#
+
+
+# Use rand scores to compare cluster membership
+k = 2
+repetitions=1
+kmedoids = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+kmedoids_no_name = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_name)
+score = rand_score(kmedoids.labels_, kmedoids_no_name.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
+print("\nRand Score - clusters vs clusters without area name {}".format(score))
+
+kmedoids_no_tags = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
+score = rand_score(kmedoids.labels_, kmedoids_no_tags.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
+print("\nRand Score - clusters vs clusters without tags {}".format(score))
+
+kmedoids_numeric = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
+score1 = rand_score(kmedoids.labels_, kmedoids_numeric.labels_)
+print("\nRand Score - orig clusters vs numeric only clusters: {}".format(score1)) # 0.8887329015421521
+
+kmedoids_tags = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_tags)
+score1 = rand_score(kmedoids.labels_, kmedoids_tags.labels_)
+score2 = rand_score(kmedoids_tags.labels_, kmedoids_numeric.labels_)
+print("\nRand Score - orig clusters vs tags only clusters: {}".format(score1)) # 0.
+print("Rand Score - clusters tags vs numeric only clusters: {}".format(score2)) # 1.0
+
+k = 3
+repetitions=1
+kmedoids = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+kmedoids_no_name = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_name)
+score = rand_score(kmedoids.labels_, kmedoids_no_name.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
+print("\nRand Score - clusters vs clusters without area name {}".format(score))
+
+kmedoids_no_tags = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
+score = rand_score(kmedoids.labels_, kmedoids_no_tags.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
+print("\nRand Score - clusters vs clusters without tags {}".format(score))
+
+kmedoids_numeric = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
+score1 = rand_score(kmedoids.labels_, kmedoids_numeric.labels_)
+print("\nRand Score - orig clusters vs numeric only clusters: {}".format(score1)) # 0.8887329015421521
+
+kmedoids_tags = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_tags)
+score1 = rand_score(kmedoids.labels_, kmedoids_tags.labels_)
+score2 = rand_score(kmedoids_tags.labels_, kmedoids_numeric.labels_)
+print("\nRand Score - orig clusters vs tags only clusters: {}".format(score1)) # 0.
+print("Rand Score - clusters tags vs numeric only clusters: {}".format(score2)) # 1.0
 
 # Use Logistic Regression to identify which parameters have the most effect on cluster membership
-k=2
-repetitions=1
-distances = gower_distance(dfCityKerbsTags, tags_cols, categorical_cols, numerical_cols)
-kmedoids = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
-
+k=3
 # Covert categorical cols to dummy variables and merge back into dataset
 dfLogit = dfCityKerbsTags.reindex(columns = tags_cols+categorical_cols+numerical_cols)
 for col in categorical_cols:
@@ -455,15 +528,14 @@ Ycol = 'cluster'
 Xcols = [c for c in dfLogit.columns if c != Ycol]
 X = dfLogit.loc[:, Xcols]
 Y = dfLogit.loc[:, Ycol]
-#logreg = sm.Logit(Y.astype(float), X.astype(float)).fit() # Fails to converge
+try:
+    logreg = sm.Logit(Y.astype(float), X.astype(float)).fit() # Fails to converge
+    print("Cluster All Cols Regression Model")
+    print(logreg.summary())
+except Exception as err:
+    print("Cluster All Cols failed to converge")
 
-# try sklearn also - Also fails to converge
-#sklogit = linear_model.LogisticRegression().fit(np.asarray(X), np.asarray(Y))
-
-
-# Look at correlations to see if these are preventing logit model from converging
-from matplotlib import pyplot as plt
-import seaborn as sn
+# Use correlation plot to check for correlations between variables.
 f, ax = plt.subplots(1,1, figsize = (10,10))
 corr = dfLogit.loc[:, Xcols].corr()
 sn.heatmap(corr, annot=True, ax=ax)
@@ -472,12 +544,6 @@ sn.heatmap(corr, annot=True, ax=ax)
 #
 # Cluster without area name columns
 #
-categorical_cols = ['is_polygon']
-
-distances_no_name = gower_distance(dfCityKerbsTags, tags_cols, categorical_cols, numerical_cols)
-kmedoids_no_name = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_name)
-score = rand_score(kmedoids.labels_, kmedoids_no_name.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
-print("Rand Score - clusters vs clusters without area name {}".format(score))
 
 # Logit model of cluster excluding tags, does this converge?
 dfLogit['cluster_no_name'] = kmedoids_no_name.labels_
@@ -498,13 +564,6 @@ except Exception as err:
 #
 # Cluster without tags columns
 #
-categorical_cols = ['is_polygon', 'area_name']
-
-distances_no_tags = gower_distance(dfCityKerbsTags, None, categorical_cols, numerical_cols)
-kmedoids_no_tags = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
-score = rand_score(kmedoids.labels_, kmedoids_no_tags.labels_) # 0.8000758172981858 area name doesn't affect cluster much, but does effect more than tags
-print("Rand Score - clusters vs clusters without tags {}".format(score))
-
 # Logit model of cluster excluding tags, does this converge?
 dfLogit['cluster_no_tags'] = kmedoids_no_tags.labels_
 
@@ -522,41 +581,10 @@ except Exception as err:
 
 
 #
-# Cluster without tag columns or area name to identify effect of the tags on determining cluster membership
-#
-categorical_cols = ['is_polygon']
-distances_no_tags_name = gower_distance(dfCityKerbsTags, None, categorical_cols, numerical_cols)
-kmedoids_no_tags_name = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags_name)
-score = rand_score(kmedoids.labels_, kmedoids_no_tags_name.labels_) # 0.8887329015421521. Clusters are predominantly determined by the non-tag cols - just 4 variables.
-print("Rand Score - clusters vs without tags or name {}".format(score))
-
-# Logit model of cluster excluding tags, does this converge?
-dfLogit['cluster_no_tags_name'] = kmedoids_no_tags_name.labels_
-
-Ycol2 = 'cluster_no_tags_name'
-Xcols = [c for c in dfLogit.columns if ('cluster' not in c) & (c not in tags_cols) & ('area_name' not in c)]
-X = dfLogit.loc[:, Xcols]
-Y = dfLogit.loc[:, Ycol]
-
-try:
-    logreg = sm.Logit(Y.astype(float), X.astype(float)).fit()
-    print("\nCluster No Tags Name Regression Model")
-    print(logreg.summary())
-except Exception as err:
-    print("Cluster No Tags Name failed to converge")
-
-
-#
 # Cluster with only numerical columns and again compare cluster membership
 # Find that categorical columns appear to make no difference to cluster membership
 # Can conclude that cluster membership dominated by area and length parameters. Could this be a scalling issue?
 #
-distances_numeric = gower_distance(dfCityKerbsTags, None, [], numerical_cols)
-kmedoids_numeric = KMedoids(n_clusters=k, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances_no_tags)
-score1 = rand_score(kmedoids.labels_, kmedoids_numeric.labels_)
-score2 = rand_score(kmedoids_no_tags_name.labels_, kmedoids_numeric.labels_)
-print("Rand Score - orig clusters vs numeric only clusters: {}".format(score1)) # 0.8887329015421521
-print("Rand Score - clusters no tags name vs numeric only clusters: {}".format(score2)) # 1.0
 
 # Again, logit regression to estimate cluster influence
 dfLogit['cluster_numeric'] = kmedoids_numeric.labels_
@@ -572,6 +600,52 @@ try:
     print(logreg.summary())
 except Exception as err:
     print("Cluster Numeric failed to converge")
+
+
+# Visualise clusters against area and length attributes
+# - plot for 2 and 3 clusters
+
+
+kmedoids2 = KMedoids(n_clusters=2, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+kmedoids3 = KMedoids(n_clusters=3, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+kmedoids7 = KMedoids(n_clusters=7, metric='precomputed', method='pam', init='random', max_iter=300, random_state=repetitions*k).fit(distances)
+
+df = dfCityKerbsTags.reindex(columns = numerical_cols)
+
+cluster_cols = ['cluser2', 'cluster3', 'cluster7']
+df[cluster_cols[0]] = kmedoids2.labels_
+df[cluster_cols[1]] = kmedoids3.labels_
+df[cluster_cols[2]] = kmedoids7.labels_
+
+f, axs = plt.subplots(1,3, figsize = (20,10))
+
+# Plot each species
+for j, cluster_col in enumerate(cluster_cols):
+    # Set the color map to match the number of species
+    uniq = list(set(df[cluster_col]))
+    z = range(1,len(uniq))
+    hot = plt.get_cmap('Set1')
+    cNorm  = colors.Normalize(vmin=0, vmax=len(uniq))
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=hot)
+    for i in range(len(uniq)):
+        indx = df[cluster_col] == uniq[i]
+        axs[j].scatter(df.loc[indx, numerical_cols[0]], df.loc[indx, numerical_cols[1]], s=15, color=scalarMap.to_rgba(i), label=uniq[i])
+        axs[j].set_xlabel(numerical_cols[0])
+        axs[j].set_ylabel(numerical_cols[1])
+        axs[j].legend(loc='upper right')
+f.show()
+f.savefig("..\\cluster_scatter_plot.png")
+
+
+#
+#
+# Comments:
+# - area name contributes to clustering more than tags.
+# - area name provides same information as tags, means that taging is idiosyncratic to each area - useful finding.
+# - - this could maybe be more clearly demonstrated with PCA, then compare to area names
+# - clustering dominated by numeric fields. Could visulise this easily.
+# - - visualisation is not very clear. Doesn't look like there are very clear clusters
+
 ###########################
 #
 #
@@ -580,10 +654,40 @@ except Exception as err:
 # Perhaps can make sense of Tag infor using: PCA; other metrics of similarity
 #
 ###########################
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 # Single tag vector for each attribute
+tags = dfLogit[tags_cols]
+X = StandardScaler().fit_transform(tags)
+rand_st_int = 10 
+pca = PCA(random_state=rand_st_int)
+tag_new_components = pca.fit_transform(tags)
 
-# Reduce using PCA
+print('Explained variance ratio of each component:')
+print(pca.explained_variance_ratio_)
+
+print('Eigenvalues of each component:')
+print(pca.explained_variance_)
+
+dfPCA = pd.DataFrame(pca.components_, columns = tags_cols)
+
+dfScree = pd.DataFrame({'eigenvalue':pca.explained_variance_, 'PC':list(range(1, pca.n_components_ + 1))})
+ax = dfScree.plot.line(x = 'PC', y = 'eigenvalue') #  Based on scree plot would choose 3-4 components#
+ax.set_xticks(range(dfPCA.shape[0]))
+ax.get_figure().show()
+
 
 # Interpret principal components
+# - how to connect to  area names?
+# - perhaps better to cluster again using PCA components
 
+# Interpret top 5 compoents
+f, ax = plt.subplots(figsize = (20,10))
+PCA5 = dfPCA.loc[:4]
+im = ax.imshow(PCA5.values, cmap = 'RdBu', vmin = -1*abs(PCA5).max().max(), vmax = abs(PCA5).max().max())
+ax.set_xticks(range(len(PCA5.columns)))
+ax.set_xticklabels(PCA5.columns, rotation = 45)
+f.colorbar(im, shrink = 0.5)
+f.show()
+f.savefig("..\\images\\tags_pca_grid.png")
