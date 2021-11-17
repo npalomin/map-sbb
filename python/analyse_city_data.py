2006017aff9176452fa1772fdfffa4d7a0eb8b73
Obi Sargoni <othompsarg@hotmail.com>
2021-11-17 11:56:20 +0000

    New script for calculating footway coverage of multiple cities. Initial results and figure included.

---
+++ python/analyse_city_data.py
@@ -0,0 +1,116 @@
+# Script to load and analyse OSM footways, roads and curbs data
+
+import os
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from unidecode import unidecode
+
+import osm_utils as osmu
+import importlib
+importlib.reload(osmu)
+
+from matplotlib import pyplot as plt
+
+
+#############################
+#
+#
+# Globals
+#
+#
+#############################
+merc_crs = {'init' :'epsg:3857'}
+output_dir = "..//data//world"
+
+
+############################
+#
+#
+# Load cities to get data for
+#
+#
+############################
+
+countries = ['United Kingdom', 'France', 'Spain', 'Japan', 'Germany', 'China', 'United States of America', 'Columbia', 'Chile', 'Iraq', 'Egypt']
+
+# Load world cities data, access from https://data.london.gov.uk/dataset/global-city-population-estimates
+dfCityPop = pd.read_csv("../data/world/global-city-population-estimates.csv", encoding = 'latin')
+
+# Filter to select just top n cities per country
+n = 2
+dfCityPop = dfCityPop.groupby("Country or area").apply(lambda df: df.sort_values(by='2020').iloc[:min(df.shape[0], n)])
+
+# Clean city names
+dfCityPop['city_name'] = dfCityPop['Urban Agglomeration'].map(lambda s: unidecode(s))
+
+dfCityPop['nm_cntry'] = dfCityPop['city_name'] + ", " + dfCityPop['Country or area']
+cities = dfCityPop.loc[ dfCityPop['Country or area'].isin(countries), 'nm_cntry'].values
+
+
+#############################
+#
+#
+# Load data and do something with it
+#
+#
+############################
+
+city_footways = osmu.load_city_data(cities, 'footways.gpkg', output_dir, merc_crs)
+city_roads = osmu.load_city_data(cities, 'roads.gpkg', output_dir, merc_crs)
+
+
+###########################
+#
+#
+# Calculate footway mapping potential
+#
+#
+###########################
+
+# Initialise data frame
+columns = ['city_name', 'footway_length', 'roads_length']
+city_lengths = []
+
+# Loop through cities
+for city_name in cities:
+	footways_result = city_footways[city_name]
+	roads_result = city_roads[city_name]
+
+	if (footways_result['data'] is None) | (roads_result['data'] is None):
+		continue
+
+	gdfFootways = footways_result['data']
+	gdfRoads = roads_result['data']
+
+	# Get total lengths of each
+	footway_length = gdfFootways['geometry'].length.sum()
+	roads_length = gdfRoads['geometry'].length.sum()
+
+	# Add to dataframe
+	city_lengths.append([city_name, footway_length, roads_length])
+
+
+dfLengths = pd.DataFrame(city_lengths, columns = columns)
+
+
+dfLengths['footway_coverage'] = dfLengths.apply(lambda row: row['footway_length'] / (2*row['roads_length']), axis=1)
+dfLengths.sort_values(by = 'footway_coverage', ascending=False, inplace=True)
+
+dfLengths.to_csv(os.path.join(output_dir, 'footway_coverage.csv'), index=False)
+
+# Make a figure
+def bar_chart(series, series_label, ylabel, title, img_path):
+    f, ax = plt.subplots(figsize = (10,10))
+    p1 = ax.bar(series.index, series, 0.9, label=series_label)
+    ax.axhline(0, color='grey', linewidth=0.8)
+    ax.set_ylabel(ylabel)
+    ax.set_xticks(range(len(series.index)))
+    ax.set_xticklabels(series.index)
+    ax.set_title(title)
+    plt.xticks(rotation=30, ha='right')
+    f.savefig(img_path)
+    return f, ax
+
+dfLengths.set_index('city_name', inplace=True)
+f, ax = bar_chart(dfLengths['footway_coverage'], 'footway_coverage', 'proportion of footway potential mapped', 'Footway Coverage', "..\\images\\footway_coverage.png")
