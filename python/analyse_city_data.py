c53f9a4e9de02fc0a68b5f000477740797cf6f39
Obi Sargoni <othompsarg@hotmail.com>
2022-03-23 09:46:39 +0000

    Updates to the violin plot figure

--- python/analyse_city_data.py
+++ python/analyse_city_data.py
@@ -12,6 +12,7 @@ importlib.reload(osmu)
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
 import matplotlib.image as mpimg
+import seaborn as sns
 
 
 #############################
@@ -84,10 +85,10 @@ search_term_to_group = gdfTC.set_index('TCITY15NM')['group'].to_dict()
 #
 #
 #############################
-dfFiles = osmu.available_city_data(cities, output_dir, ext = None)
-dfFiles.to_csv(os.path.join(output_dir, 'file_downloaded.csv'), index=False)
+#dfFiles = osmu.available_city_data(cities, output_dir, ext = None)
+#dfFiles.to_csv(os.path.join(output_dir, 'file_downloaded.csv'), index=False)
 
-missing_roads_and_footways_data = dfFiles.loc[ (dfFiles['walk_geometries.gpkg'].isnull()) & (dfFiles['fs_geometries.gpkg'].isnull()), 'city'].values
+#missing_roads_and_footways_data = dfFiles.loc[ (dfFiles['walk_geometries.gpkg'].isnull()) & (dfFiles['fs_geometries.gpkg'].isnull()), 'city'].values
 
 #############################
 #
@@ -106,6 +107,8 @@ dataset_names = ['roads','walk_network','footways','sidewalks','no_sidewalks', '
 #
 #
 ###########################
+'''
+
 # Initialise data frame
 dfTotal = pd.DataFrame()
 
@@ -182,6 +185,7 @@ dfTotal['walk_geometries_coverage'] = dfTotal['walk_geometries_length'] / dfTota
 dfTotal.sort_values(by = 'footways_coverage', ascending=False, inplace=True)
 
 dfTotal.to_csv(os.path.join(output_dir, coverage_file_name), index=False)
+'''
 ###############################
 #
 #
@@ -194,23 +198,23 @@ dfTotal = pd.read_csv(os.path.join(output_dir, coverage_file_name))
 
 # Make a figure
 def bar_chart(series, series_label, ylabel, title, img_path):
-    f, ax = plt.subplots(figsize = (10,10))
-    p1 = ax.bar(series.index, series, 0.9, label=series_label)
-    ax.axhline(0, color='grey', linewidth=0.8)
-    ax.set_ylabel(ylabel)
-    ax.set_xticks(range(len(series.index)))
-    ax.set_xticklabels(series.index)
-    ax.set_title(title)
-    plt.xticks(rotation=30, ha='right')
-    f.savefig(img_path)
-    return f, ax
+	f, ax = plt.subplots(figsize = (10,10))
+	p1 = ax.bar(series.index, series, 0.9, label=series_label)
+	ax.axhline(0, color='grey', linewidth=0.8)
+	ax.set_ylabel(ylabel)
+	ax.set_xticks(range(len(series.index)))
+	ax.set_xticklabels(series.index)
+	ax.set_title(title)
+	plt.xticks(rotation=30, ha='right')
+	f.savefig(img_path)
+	return f, ax
 
 df = dfTotal.set_index('city_name')
 img_path = os.path.join(img_dir, "urban_access_footways_coverage_walking_network.png")
 f, ax = bar_chart(df['footways_coverage'], 'footways_coverage', 'proportion of footway potential mapped', 'Footway Coverage', img_path)
 
 
-def violin_plot(df, data_cols, title, img_path, city_group_dict, figsize = (10,10), axes_bbox = [0,0,1,1], labelsize = 14, titlesize = 20, pt_size=20, legend_title = None):
+def violin_plot(df, data_cols, title, img_path, city_group_dict, figsize = (10,10), axes_bbox = [0,0,1,1], labelsize = 14, legend_size = 15, titlesize = 20, pt_size=20, legend_title = None, jitter = False):
 	f = plt.figure(figsize = figsize)
 	ax = f.add_axes(axes_bbox)
 
@@ -219,7 +223,11 @@ def violin_plot(df, data_cols, title, img_path, city_group_dict, figsize = (10,1
 	data = df[data_cols].values
 	pos = range(len(data_cols))
 
-	ax.violinplot(data, pos, points=20, widths=0.9, showmeans=False, showextrema=True, showmedians=False, bw_method=0.5)
+	parts = ax.violinplot(data, pos, points=20, widths=0.9, showmeans=False, showextrema=True, showmedians=False, bw_method=0.5)
+	for pc in parts['bodies']:
+		pc.set_facecolor('#999999')
+		#pc.set_edgecolor('black')
+		#pc.set_alpha(1)
 
 	# create second axis for scatter plot
 	ax2 = ax.twinx()
@@ -230,19 +238,28 @@ def violin_plot(df, data_cols, title, img_path, city_group_dict, figsize = (10,1
 	df_scatter.rename(columns = {0:'y'}, inplace=True)
 	df_scatter['group'] = df_scatter['city_name'].replace(city_group_dict)
 
-	colors = ['red','green','blue','yellow','orange', 'grey']
+	colors = ['#6b99b3','#4d9494','#004c67','#14155c','orange', 'grey']
 	groups = df_scatter['group'].unique()
 	x_displacements = np.linspace(-0.2, 0.2, len(groups))
 	for g, c, xi in zip(groups, colors, x_displacements):
 		dfgroup = df_scatter.loc[ df_scatter['group']==g]
-		ax2.scatter(dfgroup['x']+xi, dfgroup['y'], color = c, alpha = 0.5, label = g, s = pt_size)
+		#print(dfgroup['x']+xi)
+		#sns.stripplot(ax=ax2, x=dfgroup['x']+xi, y=dfgroup['y'], color=c, alpha = 0.5, size = pt_size, jitter=0.01)
+		#sns.swarmplot(ax=ax2, x=dfgroup['x']+xi, y=dfgroup['y'], color=c, alpha = 0.5, size = pt_size, orient='v')
+		x = dfgroup['x']+xi
+		if jitter:
+			x = [i+ ((np.random.random()-0.5)/50) for i in x]
+		ax2.scatter(x, dfgroup['y'], color = c, alpha = 0.5, label = g, s = pt_size)
 	
-	ax2.legend(title=legend_title)
-	ax2.set_axis_off()
+	lg = ax2.legend(prop={'size': legend_size})
+	lg.set_title(legend_title, prop={'size': legend_size})
+	##ax2.set_axis_off()
 
 	ax.set_xticks(pos)
 	ax.set_xticklabels(i.replace("_"," ").title() for i in data_cols)
-	ax.tick_params(labelsize = labelsize)
+	ax.tick_params(axis='x', labelsize = labelsize)
+	ax.tick_params(axis='y', labelsize = labelsize-5)
+	ax2.tick_params(axis='y', labelsize = labelsize-5)
 	ax.set_title(title, fontsize=20)
 
 	if img_path is not None:
@@ -301,29 +318,32 @@ def inset_figure(f, ax, df_scatter, city_group_dict, cities, inset_positions, zo
 
 
 data_cols = ['footways_coverage', 'sidewalks_coverage', 'no_sidewalks_coverage']
+df = dfTotal.reindex(columns = ['city_name', 'footways_coverage', 'all_sidewalks_coverage']).rename(columns = {'all_sidewalks_coverage':'sidewalks_coverage'})
+data_cols = ['footways_coverage', 'sidewalks_coverage']
+
+'''
 img_path = os.path.join(img_dir, "coverage_distributions.png")
 img_path_pop = os.path.join(img_dir, "coverage_distributions_groupbypop.png")
 f, ax = violin_plot(dfTotal, data_cols, None, img_path, search_term_to_group, figsize = (10,10), pt_size=20)
 f, ax = violin_plot(dfTotal, data_cols, None, img_path_pop, search_term_to_popquant, figsize = (10,10), pt_size=20, legend_title = "Population")
 
-df = dfTotal.reindex(columns = ['city_name', 'footways_coverage', 'all_sidewalks_coverage']).rename(columns = {'all_sidewalks_coverage':'sidewalks_coverage'})
-data_cols = ['footways_coverage', 'sidewalks_coverage']
+
 img_path = os.path.join(img_dir, "coverage_distributions_all_sidewalk_combined.png")
 img_path_pop = os.path.join(img_dir, "coverage_distributions_all_sidewalk_combined_groupbypop.png")
 f, ax = violin_plot(df, data_cols, None, img_path, search_term_to_group, figsize = (10,10), pt_size=20)
-f, ax = violin_plot(df, data_cols, None, img_path_pop, search_term_to_popquant, figsize = (10,10), pt_size=20, legend_title = "Population")
+f, ax = violin_plot(df, data_cols, None, img_path_pop, search_term_to_popquant, figsize = (10,10), axes_bbox= [0.1,0.1,0.8,0.75], pt_size=20, legend_title = "Population")
 
 
 # Illustrate which cities have higest coverage
 img_path = os.path.join(img_dir, "coverage_distributions_all_sidewalk_combined_groupbypop_annotated.png")
 annotate_figure(f, ax, df, search_term_to_popquant, img_path, (0.05,0.1), cities = None, data_col = "footways_coverage")
-
+'''
 
 # Add inset to show the street network of a particular city
 img_path = os.path.join(img_dir, "coverage_distributions_all_sidewalk_combined_groupbypop_imginset.png")
 cities = ['London, England', 'San Jose, United States']
-f, ax = violin_plot(df, data_cols, None, None, search_term_to_popquant, figsize = (15,20), axes_bbox= [0.1,0,0.8,0.75], pt_size=20, legend_title = "Population")
-inset_figure(f, ax, df, search_term_to_popquant, cities, [(0.2,0.7), (0.6, 0.8)], 0.35, img_dir, img_path, data_col = "footways_coverage")
+f, ax = violin_plot(df, data_cols, None, None, search_term_to_popquant, figsize = (30,30), axes_bbox= [0.3,0.05,0.5,0.8], labelsize = 30, legend_size = 20, pt_size=250, legend_title = "Population")
+inset_figure(f, ax, df, search_term_to_popquant, cities, [(0.1,0.2), (0.1, 0.55)], 0.5, img_dir, img_path, data_col = "footways_coverage")
 
 
 ############################
@@ -362,9 +382,9 @@ ax.set_xticklabels(corr_cols)
 ax.set_yticklabels(corr_cols)
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 for i in range(len(corr_cols)):
-    for j in range(len(corr_cols)):
-        text = ax.text(j, i, FootwayCoor[i, j],
-                       ha="center", va="center", color="w")
+	for j in range(len(corr_cols)):
+		text = ax.text(j, i, FootwayCoor[i, j],
+					   ha="center", va="center", color="w")
 
 img_path = os.path.join(img_dir, "accessibility_correlation.png")
 f.savefig(img_path)
